309229f727d623ea3b8a3118c9ab7c22adb7977a
Katie Liu <katieliu59@gmail.com>
2023-03-06 22:50:03 -0500

    Implement play definition

--- main.rb
+++ main.rb
@@ -2,7 +2,9 @@ require "./players"
 require "./questions"
 
 class Game 
-    def Initialize 
+    attr_accessor :player1, :player2, :current_player, :question, :winner
+
+    def initialize ()
         puts "Welcome to TwO-O-Player Math game! Let's test your knowledge!"
         @player1 = Players.new('Gordon')
         @player2 = Players.new('Ramsay')
@@ -11,11 +13,37 @@ class Game
         @winner = false
     end
 
-    def play
+    def play()
+        while !self.winner do
+            puts "#{self.current_player.name}, your turn!"
+            self.question.question
 
+            # check answer, if true -> continue, if false -> lose 1 life 
+            if self.question.check_answer? then
+                puts "Correct :)"
+            else 
+                puts "Wrong answer :("
+                self.current_player.lose_life
+            end
 
+            # check player lives, if 0 -> set winner to true
+            if self. current_player.lives == 0
+                puts "#{self.current_player.name}, you lost..."
+                self.winner = true
+            end
+
+            # initiate new question
+            self.question = Questions.new()
+
+            # add toggle between player 1 and player 2 after each round
+            if self.current_player == self.player1 then
+                self.current_player = self.player2
+            else 
+            self.current_player = self.player1
+            end
+        end
     end
-
 end
 
+Game.new().play()
         
